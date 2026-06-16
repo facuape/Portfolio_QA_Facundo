270ae1d9245fb1ede5e97f587d6599171858f94e
facuape734 <elbacuapecechea@gmail.com>
2026-06-16 04:31:00 -0300

    agrego tests login fallido y usuario bloqueado

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,3 +9,21 @@ test('login exitoso con usuario válido', async ({ page }) => {
   
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
+test('login fallido con usuario incorrecto', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  
+  await page.fill('#user-name', 'usuario_falso');
+  await page.fill('#password', 'password_falso');
+  await page.click('#login-button');
+  
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+test('login fallido con usuario bloqueado', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  
+  await page.fill('#user-name', 'locked_out_user');
+  await page.fill('#password', 'secret_sauce');
+  await page.click('#login-button');
+  
+  await expect(page.locator('[data-test="error"]')).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+});
